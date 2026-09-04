77a43f5286922cebc1c84d1faae2b48de8748588
MostafaMnesey <mneseym@gmail.com>
2026-09-04 03:07:06 +0300

    Revert "refactor: migrate homework teacherId to userId with data preservation and validation"
    This reverts commit f8bbb4a8a95851a7d08c078fe7aff4a7b7fe6f0e.

--- prisma/migrations/20260903131038_replace_homework_teacher_with_user/migration.sql
+++ prisma/migrations/20260903131038_replace_homework_teacher_with_user/migration.sql
@@ -1,55 +1,22 @@
 /*
-  Replace homework.teacherId (teacher.id)
-  with homework.userId (user.id)
+  Warnings:
+
+  - You are about to drop the column `teacherId` on the `homework` table. All the data in the column will be lost.
+  - Added the required column `userId` to the `homework` table without a default value. This is not possible if the table is not empty.
+
 */
+-- DropForeignKey
+ALTER TABLE "homework" DROP CONSTRAINT "homework_teacherId_fkey";
 
--- Drop old foreign key
-ALTER TABLE "homework"
-DROP CONSTRAINT "homework_teacherId_fkey";
-
--- Drop old index
+-- DropIndex
 DROP INDEX "homework_teacherId_idx";
 
--- Add userId as nullable first
-ALTER TABLE "homework"
-ADD COLUMN "userId" TEXT;
+-- AlterTable
+ALTER TABLE "homework" DROP COLUMN "teacherId",
+ADD COLUMN     "userId" TEXT NOT NULL;
 
--- Migrate existing data:
--- homework.teacherId -> teacher.id -> teacher.user_id -> user.id
-UPDATE "homework" h
-SET "userId" = t."user_id"
-FROM "teacher" t
-WHERE h."teacherId" = t."id";
+-- CreateIndex
+CREATE INDEX "homework_userId_idx" ON "homework"("userId");
 
--- Verify that every homework got a userId
--- This should result in 0
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM "homework"
-        WHERE "userId" IS NULL
-    ) THEN
-        RAISE EXCEPTION 'Some homework records could not be mapped to a teacher user';
-    END IF;
-END $$;
-
--- Now make userId required
-ALTER TABLE "homework"
-ALTER COLUMN "userId" SET NOT NULL;
-
--- Remove old teacherId
-ALTER TABLE "homework"
-DROP COLUMN "teacherId";
-
--- Create new index
-CREATE INDEX "homework_userId_idx"
-ON "homework"("userId");
-
--- Add new foreign key
-ALTER TABLE "homework"
-ADD CONSTRAINT "homework_userId_fkey"
-FOREIGN KEY ("userId")
-REFERENCES "user"("id")
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE "homework" ADD CONSTRAINT "homework_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
